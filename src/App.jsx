2777813b521ca7a4aba1322e8a3b18517a27abdd
CraftsManTester <august080829@naver.com>
2025-05-15 04:01:00 +0900

    docs :: 테스트용 주석 추가

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Route path = '/docs' element = {<DocsPage></DocsPage>}></Route>
       <Route path = '/plus' element = {<PlusPage></PlusPage>}></Route>
     </Routes>
-  );
+  ); //test
 }
 
 export default App;
